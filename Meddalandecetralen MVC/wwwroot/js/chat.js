d8d7abde264d7feb8e68e18778d511549586618a
argunho <aslan_argun@hotmail.com>
2020-10-23 23:32:21 +0200

    update

---
+++ Meddalandecetralen MVC/wwwroot/js/chat.js
@@ -0,0 +1,34 @@
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+
+//Disable send button until connection is established
+document.getElementById("sendButton").disabled = true;
+
+connection.on("ReceiveMessage", function (user, message) {
+    var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    var encodedMsg = "<span class='user-name'>" + user + "<span/><span class='user-text'>" + msg + "</span>";
+    var li = document.createElement("li");
+    //li.textContent = encodedMsg;
+    li.innerHTML = encodedMsg;
+    document.getElementById("messagesList").appendChild(li);
+    document.getElementById("startChat").appendChild(li);
+});
+
+connection.start().then(function () {
+    document.getElementById("sendButton").disabled = false;
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+document.getElementById("sendButton").addEventListener("click", function (event) {
+
+    hideBlock(); // Hide start block
+
+    var user = document.getElementById("userInput").value;
+    var message = document.getElementById("messageInput").value;
+    connection.invoke("SendMessage", user, message).catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
---
+++ Meddalandecetralen MVC/wwwroot/js/site.js
@@ -0,0 +1,48 @@
+﻿"use strict";
+
+if (location.pathname == "/chat")
+ getChat();
+
+function startChat() {
+    $('#body').load('/chat');
+    getChat();
+}
+
+function getChat() {
+    setTimeout(function () {
+        $("#loading").fadeOut();
+        //setTimeout(function () {
+        //    var script = document.createElement("script");
+        //    script.type = "text/javascript";
+        //    script.src = "~/js/chat.js";
+        //    document.getElementById("chat").appendChild(script);
+        //    return false;
+        //}, 500)
+        setTimeout(function () {
+            $("#block").removeClass("none");
+        }, 1000)
+    }, 1000);
+}
+
+function hideBlock() {
+    var hidden = $("#block").hasClass("text-block");
+    console.log(hidden)
+    if (!hidden) {
+        $("#block").addClass("none");
+        $("#block").addClass("text-block");
+        $("#block").removeClass("chat-start");
+        $("#userInput").fadeOut();
+        $("#sendButton").html("Skicka");
+        $("textarea").html("");
+        $("label.label").each(function () {
+            $(this).addClass("none");
+        })
+        setTimeout(function () {
+             $("#block, #msg").removeClass("none");
+        }, 500)
+    }
+}
+
+function newChat() {
+    console.log("newchat")
+}
